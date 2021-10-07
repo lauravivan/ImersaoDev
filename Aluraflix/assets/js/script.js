1084606bf33f01823c93ca600bfc87def2bf0539
Laura <lauravivangoncalves@gmail.com>
2021-10-07 12:09:08 -0300

    Inclusão aluraflix

---
+++ Aluraflix/assets/js/script.js
@@ -0,0 +1,489 @@
+moviesList = ["edward scissorhands", "assets/img/movies/edward-scissorhands.jpg", "the suicide squad", "assets/img/movies/the-suicide-squad.jpg", "promising young woman", "assets/img/movies/promising-young-woman.jpg", "scott pilgrim vs the world", "assets/img/movies/scott-pilgrim.jpg", "eternal sunshine of the spotless mind", "assets/img/movies/eternal-sunshine.jpg", "coraline", "assets/img/movies/coraline-2.jpg", "ghostbusters", "assets/img/movies/ghostbusters.jpg", "spirited away", "assets/img/movies/spirited-away.jpg", "cruella", "assets/img/movies/cruella-2.jpg", "joker", "assets/img/movies/joker.jpg", "inception", "assets/img/movies/inception.jpg", "the matrix", "assets/img/movies/the-matrix.jpg", "the angry birds movie", "assets/img/movies/angry-birds.jpg", "kung fu panda", "assets/img/movies/kung-fu-panda.jpg", "hidden figures", "assets/img/movies/hidden-figures.jpg", "monster house", "assets/img/movies/monster-house.jpg", "corpse bride", "assets/img/movies/corpse-bride.jpg", "ferris bueller's day off", "assets/img/movies/ferris.jpg", "willy wonka and the chocolate factory", "assets/img/movies/willy-wonka.jpg", "the truman show", "assets/img/movies/the-truman-show.jpg", "the trial of the chicago seven", "assets/img/movies/the-trial-of-the-chicago-7.jpg", "thelma and louise", "assets/img/movies/thelma-and-louise.jpg", "spider man: into the spider verse", "assets/img/movies/spider-man.jpg", "catch me if you can", "assets/img/movies/catch-me-if-you-can.jpg", "good will hunting", "assets/img/movies/good-will-hunting.jpg", "elis", "assets/img/movies/elis.jpg", "green book", "assets/img/movies/green-book.jpg", "her", "assets/img/movies/her.jpg", "the king's speech", "assets/img/movies/the-kings-speech.jpg", "harry potter and the sorcerer's stone", "assets/img/movies/harry-potter.jpg"];
+
+seriesList = ["stranger things", "assets/img/series/stranger-things.jpg", "sherlock", "assets/img/series/sherlock.jpg", "skins", "assets/img/series/skins.jpg", "the queen's gambit", "assets/img/series/queen-s-gambit.jpg", "two broke girls", "assets/img/series/two-broke-girls.jpg", "friends", "assets/img/series/friends.jpg", "thirteen reasons why", "assets/img/series/13-reasons-why.png", "the office", "assets/img/series/the-office.jpg", "the middle", "assets/img/series/the-middle-2.jpg", "new girl", "assets/img/series/new-girl.jpg", "victorious", "assets/img/series/victorious.jpg", "skam", "assets/img/series/skam.jpg", "skam austin", "assets/img/series/skam-austin.jpg", "the end of the fucking world", "assets/img/series/the-end-of-the-fucking-world.jpg", "austin and ally", "assets/img/series/austin-and-ally.jpg"]
+
+cartoonsList = ["the amazing world of gumball", "assets/img/cartoons/gumball.jpg", "regular show", "assets/img/cartoons/regular-show.jpg", "courage the cowardly dog", "assets/img/cartoons/courage.jpg", "foster's home for imaginary friends", "assets/img/cartoons/fosters.jpg", "the simpsons", "assets/img/cartoons/simpsons.jpg", "the marvelous misadventures of flapjack", "assets/img/cartoons/flapjack.jpg", "the grim adventures of billy and mandy", "assets/img/cartoons/billy-and-mandy.jpg", "oswaldo", "assets/img/cartoons/oswaldo.jpg", "what's new scooby-doo?", "assets/img/cartoons/scooby-doo.jpg", "ed, edd n eddy", "assets/img/cartoons/ed-edd-eddy.jpg"]
+
+dramasList = ["guardian: the lonely and great god", "assets/img/dramas/goblin.jpg", "strong woman do bong-soon", "assets/img/dramas/strong-woman-do-bong-soon.jpg", "reply 1997", "assets/img/dramas/reply-1997.jpg", "the smile has left your eyes", "assets/img/dramas/the-smile-has-left-your-eyes.jpg", "nevertheless", "assets/img/dramas/nevertheless.jpg", "doctors", "assets/img/dramas/doctors.jpg", "cheese in the trap", "assets/img/dramas/cheese-in-the-trap.jpg", "uncontrollably fond", "assets/img/dramas/Uncontrollably-Fond.jpg", "doom at your service", "assets/img/dramas/doom-at-your-service.jpeg", "a little thing called first love", "assets/img/dramas/first-love.jpg"]
+
+animesList = ["kimi ni todoke", "assets/img/animes/kimi-ni-todoke.jpg", "hyouka", "assets/img/animes/hyouka.jpg", "another", "assets/img/animes/another.jpg", "gekkan shoujo nozaki-kun", "assets/img/animes/nozaki-kun.jpg", "tonari no kaibutsu-kun", "assets/img/animes/little-monster.jpg", "kamisama hajimemashita", "assets/img/animes/kamisama-hajimemashita.jpg", "kaichou wa maid-sama", "assets/img/animes/maid-sama.jpg", "ao haru ride", "assets/img/animes/ao-haru-ride.jpg", "given", "assets/img/animes/given.jpg", "akagami no shirayuki-hime", "assets/img/animes/akagami.jpg"]
+
+movies = [
+    {
+        title: "EDWARD SCISSORHANDS",
+        description: "His story will touch you even though he can't.",
+        photo: "assets/img/movies-photos/edward-scissorhands.png"
+    },
+    {   
+        title: "THE SUICIDE SQUAD",
+        description: "They're dying to save the world.",
+        photo: "assets/img/movies-photos/the-suicide-squad.gif"
+    },
+    {
+        title: "PROMISING YOUNG WOMAN",
+        description: "Revenge never looked so promising.",
+        photo: "assets/img/movies-photos/promising-young-woman.jpg"
+    },
+    {
+        title: "SCOTT PILGRIM VS. THE WORLD",
+        description: "An epic of epic epicness.",
+        photo: "assets/img/movies-photos/scott-pilgrim.gif"
+    },
+    {
+        title: "ETERNAL SUNSHINE OF THE SPOTLESS MIND",
+        description: "You can erase someone from your mind. Getting them out of your heart is another story.",
+        photo: "assets/img/movies-photos/eternal-sunshine.jpg"
+    },
+    {
+        title: "CORALINE",
+        description: "Be careful what you wish for.",
+        photo: "assets/img/movies-photos/coraline.gif"
+    },
+    {
+        title: "GHOSTBUSTERS",
+        description: "They're here to save the world.",
+        photo: "assets/img/movies-photos/ghostbusters.jpg"
+    },
+    {
+        title: "SEN TO CHIHIRO NO KAMIKAKUSHI",
+        description: "She will see a lot and learn a lot.",
+        photo: "assets/img/movies-photos/spirited-away.gif"
+    },
+    {
+        title: "CRUELLA",
+        description: "No one is all bad.",
+        photo: "assets/img/movies-photos/cruella.gif"
+    },
+    {
+        title: "JOKER",
+        description: "Put on a happy face.",
+        photo: "assets/img/movies-photos/joker.jpg"
+    },
+    {
+        title: "INCEPTION",
+        description: "Your mind is the scene of the crime.",
+        photo: "assets/img/movies-photos/inception.gif"
+    },
+    {
+        title: "THE MATRIX",
+        description: "Believe the unbelievable.",
+        photo: "assets/img/movies-photos/the-matrix.gif"
+    },
+    {
+        title: "THE ANGRY BIRDS MOVIE",
+        description: "Why so angry?",
+        photo: "assets/img/movies-photos/angry-birds.gif"
+    },
+    {
+        title: "KUNG FU PANDA",
+        description: "Prepare for awesomeness",
+        photo: "assets/img/movies-photos/kung-fu-panda.gif"
+    },
+    {
+        title: "HIDDEN FIGURES",
+        description: "Genius has no race. Strength has no gender. Courage has no limit.",
+        photo: "assets/img/movies-photos/hidden-figures.jpg"
+    },
+    {
+        title: "MONSTER HOUSE",
+        description: "There goes the neighborhood.",
+        photo: "assets/img/movies-photos/monster-house.jpg"
+    },
+    {
+        title: "CORPSE BRIDE",
+        description: "There's been a grave misunderstanding.",
+        photo: "assets/img/movies-photos/corpse-bride.gif"
+    },
+    {
+        title: "FERRIS BUELLER'S DAY OFF",
+        description: "One man's struggle to take it easy.",
+        photo: "assets/img/movies-photos/ferris.gif"
+    },
+    {
+        title: "WILLY WONKA AND THE CHOCOLATE FACTORY",
+        description: "It's everybody's non-pollutionary, anti-institutionary, pro-confectionery factory of fun!",
+        photo: "assets/img/movies-photos/willy-wonka.gif"
+    },
+    {
+        title: "THE TRUMAN SHOW",
+        description: "On the air, unaware.",
+        photo: "assets/img/movies-photos/the-truman-show.gif"
+    },
+    {
+        title: "THE TRIAL OF THE CHICAGO 7",
+        description: "In 1968, democracy refused to back down. Based on a true story.",
+        photo: "assets/img/movies-photos/the-trial.jpg"
+    },
+    {
+        title: "THELMA AND LOUISE",
+        description: "Somebody said get a life... so they did.",
+        photo: "assets/img/movies-photos/thelma-and-louise.jpg"
+    },
+    {
+        title: "SPIDER-MAN: INTO THE SPIDER-VERSE",
+        description: "A journey of multiverses",
+        photo: "assets/img/movies-photos/spider-man-2.gif"
+    },
+    {
+        title: "CATCH ME IF YOU CAN",
+        description: "The true story of a real fake.",
+        photo: "assets/img/movies-photos/catch-me.jpg"
+    },
+    {
+        title: "GOOD WILL HUNTING",
+        description: "For the first 20 years of his life, Will Hunting has called the shots, now he's about to meet his match.",
+        photo: "assets/img/movies-photos/good-will.gif"
+    },
+    {
+        title: "ELIS",
+        description: "Based on a true story.",
+        photo: "assets/img/movies-photos/elis.jpg"
+    },
+    {
+        title: "GREEN BOOK",
+        description: "Inspired by a true friendship.",
+        photo: "assets/img/movies-photos/green-book.gif"
+    },
+    {
+        title: "HER",
+        description: "A spike jonze love story.",
+        photo: "assets/img/movies-photos/her.jpg"
+    },
+    {
+        title: "THE KING'S SPEECH",
+        description: "It takes leadership to confront a nation's fear. It takes friendship to conquer your own.",
+        photo: "assets/img/movies-photos/the-kings-speech.jpg"
+    },
+    {
+        title: "HARRY POTTER AND THE SORCERER'S STONE",
+        description: "Journey beyond your imagination.",
+        photo: "assets/img/movies-photos/harry-potter.jpg"
+    }
+];
+
+series = [
+    {
+        title: "STRANGER THINGS",
+        photo: "assets/img/series-photos/stranger-things.gif"
+    },
+    {
+        title: "SHERLOCK",
+        photo: "assets/img/series-photos/sherlock.jpg"
+    },
+    {
+        title: "SKINS",
+        photo: "assets/img/series-photos/skins.jpg"
+    },
+    {
+        title: "THE QUEEN'S GAMBIT",
+        photo: "assets/img/series-photos/the-queens-gambit.gif"
+    },
+    {
+        title: "2 BROKE GIRLS",
+        photo: "assets/img/series-photos/two-broke-girls.gif"
+    },
+    {
+        title: "FRIENDS",
+        photo: "assets/img/series-photos/friends.jpg"
+    },
+    {
+        title: "13 REASONS WHY",
+        photo: "assets/img/series-photos/13-reasons-why.jpg"
+    },
+    {
+        title: "THE OFFICE",
+        photo: "assets/img/series-photos/the-office.jpg"
+    },
+    {
+        title: "THE MIDDLE",
+        photo: "assets/img/series-photos/the-middle.gif"
+    },
+    {
+        title: "NEW GIRL",
+        photo: "assets/img/series-photos/new-girl.jpg"
+    },
+    {
+        title: "VICTORIOUS",
+        photo: "assets/img/series-photos/victorious.jpg"
+    },
+    {
+        title: "SKAM",
+        photo: "assets/img/series-photos/skam.jpg"
+    },
+    {
+        title: "SKAM AUSTIN",
+        photo: "assets/img/series-photos/skam-austin.png"
+    },
+    {
+        title: "THE END OF THE FUCKING WORLD",
+        photo: "assets/img/series-photos/the-end.jpg"
+    },
+    {
+        title: "AUSTIN AND ALLY",
+        photo: "assets/img/series-photos/austin-and-ally.jpg"
+    }
+]
+
+cartoons = [
+    {
+        title: "THE AMAZING WORLD OF GUMBALL",
+        photo: "assets/img/cartoons-photos/gumball.gif"
+    },
+    {
+        title: "REGULAR SHOW",
+        photo: "assets/img/cartoons-photos/regular-show.gif"
+    },
+    {
+        title: "COURAGE THE COWARDLY DOG",
+        photo: "assets/img/cartoons-photos/courage.gif"
+    },
+    {
+        title: "FOSTER'S HOME FOR IMAGINARY FRIENDS",
+        photo: "assets/img/cartoons-photos/fosters.gif"
+    },
+    {
+        title: "THE SIMPSONS",
+        photo: "assets/img/cartoons-photos/simpsons.jpg"
+    },
+    {
+        title: "THE MARVELOUS MISADVENTURES OF FLAPJACK",
+        photo: "assets/img/cartoons-photos/flapjack.gif"
+    },
+    {
+        title: "THE GRIM ADVENTURES OF BILLY AND MANDY",
+        photo: "assets/img/cartoons-photos/billy-and-mandy.jpg"
+    },
+    {
+        title: "OSWALDO",
+        photo: "assets/img/cartoons-photos/oswaldo.jpg"
+    },
+    {
+        title: "WHAT'S NEW SCOOBY-DOO?",
+        photo: "assets/img/cartoons-photos/scooby-doo.gif"
+    },
+    {
+        title: "ED, EDD N EDDY",
+        photo: "assets/img/cartoons-photos/ed-edd-eddy.gif"
+    }
+]
+
+dramas = [
+    {
+        title: "GUARDIAN: THE LONELY AND GREAT GOD",
+        photo: "assets/img/dramas-photos/goblin.jpg"
+    },
+    {
+        title: "STRONG WOMAN DO BONG-SOON",
+        photo: "assets/img/dramas-photos/bong-soon.jpg"
+    },
+    {
+        title: "REPLY 1997",
+        photo: "assets/img/dramas-photos/reply-97.jpg"
+    },
+    {
+        title: "THE SMILE HAS LEFT YOUR EYES",
+        photo: "assets/img/dramas-photos/the-smile-has-left-your-eyes.jpeg"
+    },
+    {
+        title: "NEVERTHELESS",
+        photo: "assets/img/dramas-photos/nevertheless.jpg"
+    },
+    {
+        title: "DOCTORS",
+        photo: "assets/img/dramas-photos/doctors.jpg"
+    },
+    {
+        title: "CHEESE IN THE TRAP",
+        photo: "assets/img/dramas-photos/cheese-in-the-trap.jpg"
+    },
+    {
+        title: "UNCONTROLLABLY FOND",
+        photo: "assets/img/dramas-photos/uncontrollably-fond.jpg"
+    },
+    {
+        title: "DOOM AT YOUR SERVICE",
+        photo: "assets/img/dramas-photos/doom-at-your-service.jpg"
+    },
+    {
+        title: "A LITTLE THING CALLED FIRST LOVE",
+        photo: "assets/img/dramas-photos/first-love.jpg"
+    },
+]
+
+animes = [
+    {
+        title: "KIMI NI TODOKE",
+        photo: "assets/img/animes-photos/kimi-ni-todoke.gif"
+    },
+    {
+        title: "HYOUKA",
+        photo: "assets/img/animes-photos/hyouka.gif"
+    },
+    {
+        title: "ANOTHER",
+        photo: "assets/img/animes-photos/another.gif"
+    },
+    {
+        title: "GEKKAN SHOUJO NOZAKI-KUN",
+        photo: "assets/img/animes-photos/nozaki-kun.gif"
+    },
+    {
+        title: "TONARI NO KAIBUTSU-KUN",
+        photo: "assets/img/animes-photos/little-monster.gif"
+    },
+    {
+        title: "KAMISAMA HAJIMEMASHITA",
+        photo: "assets/img/animes-photos/kamisama-hajimemashita.gif"
+    },
+    {
+        title: "KAICHOU WA MAID-SAMA",
+        photo: "assets/img/animes-photos/maid-sama.gif"
+    },
+    {
+        title: "AO HARU RIDE",
+        photo: "assets/img/animes-photos/ao-haru-ride.gif"
+    },
+    {
+        title: "GIVEN",
+        photo: "assets/img/animes-photos/given.jpg"
+    },
+    {
+        title: "AKAGAMI NO SHIRAYUKI-HIME",
+        photo: "assets/img/animes-photos/akagami.gif"
+    }
+]
+
+function showMovies() {
+    showOnScreen(moviesList, "FILMES", movies);
+}
+
+function showSeries() {
+    showOnScreen(seriesList, "SÉRIES", series);
+}
+
+function showCartoons() {
+    showOnScreen(cartoonsList, "CARTOONS", cartoons);
+}
+
+function showDramas() {
+    showOnScreen(dramasList, "DORAMAS", dramas);
+}
+
+function showAnimes() {
+    showOnScreen(animesList, "ANIMES", animes);
+}
+
+let myInterval;
+function showOnScreen(list, title, group) {
+    let containerCatalog = document.getElementById("catalog");
+    let containerSlide = document.getElementById("slide");
+    containerCatalog.innerText = "";
+    containerCatalog.innerHTML = `<h3 class="catalog-title">${title}</h3>`
+    for (let i = 0; i < list.length; i++) {
+        if ((i % 2) != 0) { 
+            containerCatalog.innerHTML += `<img class="catalog-images" src="${list[i]}"> `;
+        }
+    }
+    clearInterval(myInterval);
+    index = 0;
+    containerSlide.style.backgroundImage = `url(${group[index].photo})`
+    containerSlide.innerHTML = `<h1 id="page-title">${group[index].title}</h1>`;
+    myInterval = setInterval(function() {
+        index++;
+        containerSlide.style.backgroundImage = `url(${group[index].photo})`
+        containerSlide.innerHTML = `<h1 id="page-title">${group[index].title}</h1>`;
+        if (index == (group.length - 1)) {
+            index = -1;
+        }
+    }, 4580);
+}
+
+function aluraflix() {
+    location.reload(); 
+}
+let times = 1;
+function search() {
+    clearInterval(myInterval);
+    let lists = [moviesList, seriesList, cartoonsList, dramasList, animesList];
+    let listsIndex = 0;
+    let containerCatalog = document.getElementById("catalog");
+    let containerSlide = document.getElementById("slide");
+    let userSearch = document.getElementById("search").value;
+    containerCatalog.innerText = "";
+    containerSlide.innerText = "";
+    containerSlide.style.boxShadow = "none";
+    containerSlide.style.backgroundImage = "none";
+    containerCatalog.style.background = "none";
+
+    if (times == 1) {
+        times++;
+    }
+    containerSlide.innerHTML = `<p id="search-phrase-result">Resultado(s) da sua busca:</p>`
+    let flag = -2;
+    let imgIndex;
+    let splitTitle;
+    for (let i = 0; i < lists[listsIndex].length; i++) {
+        splitTitle = lists[listsIndex][i].split(" ")
+        if ((splitTitle.includes(userSearch.toLowerCase())) || (userSearch.toLowerCase()) == lists[listsIndex][i]) {
+            flag = 1;
+            imgIndex = i + 1;
+            containerSlide.innerHTML += `<img class="search-result-img" src="${lists[listsIndex][imgIndex]}">`;
+        }
+        if (i == (lists[listsIndex].length - 1)) {
+            i = 0;
+            listsIndex++;
+        }
+        if (listsIndex == lists.length) {
+            break;
+        }
+    }
+    if (flag == -2) {
+        containerSlide.innerHTML += `<p id="no-found">Nada foi encontrado</p>`;
+    }
+}
+
+
+let categories = ["FILMES", "SÉRIES", "CARTOONS", "DORAMAS", "ANIMES"];
+let categoriesList = [moviesList, seriesList, cartoonsList, dramasList, animesList];
+let container = document.getElementById("catalog");
+let index = 0;
+let flag = 0;
+for (let i = 0; i <= 10; i++) {
+    if ((i % 2) != 0) {
+        if (flag == 0) {
+            container.innerHTML += `<h3 class="catalog-title">${categories[index]}</h3>`;
+            flag = 1;
+        }
+        container.innerHTML += `<img class="catalog-images" src="${categoriesList[index][i]}">`;
+    } 
+    if (i == 10) {
+        container.innerHTML += `<img class="not-image" onclick="showSeries()" src="assets/img/see-more.png" style="width: 11vw; height: 45vh;">`;
+        i = 0;
+        index++;
+        flag = 0;
+    }
+    if (index == 5) {
+        break;
+    }
+}
+
+index = 0; 
+indexGroup = 0;
+groups = [movies, series, cartoons, dramas, animes];
+let containerSlide = document.getElementById("slide");
+containerSlide.style.backgroundImage = `url(${groups[indexGroup][index].photo})`
+
+containerSlide.innerHTML = `<h1 id="page-title">${groups[indexGroup][index].title}</h1>`;
+
+myInterval = setInterval(function() {
+    containerSlide.style.backgroundImage = `url(${groups[indexGroup][index].photo})`
+    containerSlide.innerHTML = `<h1 id="page-title">${groups[indexGroup][index].title}</h1>`;
+    if (index == (groups[indexGroup].length - 1)) {
+        index = -1;
+        indexGroup++;
+    }
+    if (indexGroup == groups.length) {
+        indexGroup = 0;
+    }
+    index++;
+}, 4170);
